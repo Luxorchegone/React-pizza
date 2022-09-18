c27767fbff41265ebe18500e1ca21cab45019d2b
Vladimir <luxorchegone@gmail.com>
2022-09-18 15:17:18 +0300

    Добавил Skeleton(анимацию загрузки)для карточки пиццы

---
+++ src/components/PizzaSkeleton/PizzaSkeleton.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const PizzaSkeleton = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={280}
+    height={465}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <rect x="0" y="268" rx="10" ry="10" width="280" height="22" /> 
+    <circle cx="130" cy="130" r="130" /> 
+    <rect x="149" y="419" rx="23" ry="23" width="131" height="46" /> 
+    <rect x="0" y="428" rx="10" ry="10" width="92" height="27" /> 
+    <rect x="0" y="313" rx="10" ry="10" width="280" height="88" />
+  </ContentLoader>
+)
+
+export default PizzaSkeleton
+
